b4e2e164191d2ad5d9b3d95ef6712d04e9e0755e
ShinyParadise <shinodise@gmail.com>
2022-11-26 13:30:27 +0300

    Added dtos

---
+++ app/src/main/java/ru/greenlab/quiz/dto/Category.kt
@@ -0,0 +1,8 @@
+package ru.greenlab.quiz.dto
+
+import java.io.Serializable
+
+data class Category(
+    val id: String,
+    val name: String
+): Serializable
---
+++ app/src/main/java/ru/greenlab/quiz/dto/User.kt
@@ -0,0 +1,10 @@
+package ru.greenlab.quiz.dto
+
+import java.io.Serializable
+
+data class User(
+    val city: String,
+    val id: String,
+    val name: String,
+    val phone_number: String
+): Serializable
