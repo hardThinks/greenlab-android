a8c5e057ec81dcc1da0d05277adeb9fd8ee0012f
ShinyParadise <shinodise@gmail.com>
2022-11-27 14:31:54 +0300

    Cool design

--- greenlabadmin/src/main/java/ru/greenlab/greenlabadmin/MainActivity.kt
+++ greenlabadmin/src/main/java/ru/greenlab/greenlabadmin/MainActivity.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,9 +49,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AdminScreen(users: List<User>) {
-    LazyColumn(modifier = Modifier.padding(20.dp)) {
-        items(users) {
-            UserItem(it)
+    Column {
+        Spacer(modifier = Modifier.padding(top = 15.dp))
+        Header()
+        LazyColumn(modifier = Modifier.padding(horizontal = 20.dp)) {
+            items(users) {
+                UserItem(it)
+            }
         }
     }
 }
@@ -58,39 +66,64 @@ fun UserItem(user: User) {
         Column {
             Text(
                 text = user.name + ", " + user.city,
-                style = MaterialTheme.typography.h5
+                style = MaterialTheme.typography.h5,
+                fontWeight = FontWeight.Bold
             )
             Text(text = user.phone_number, fontSize = 20.sp)
             Divider()
 
-            for (item in user.quiz_results.last().quiz_result_items) {
-                Row {
-                    Text(item.category.name + ": ")
-                    Text(item.percentage.toString() + '%')
+            if (user.quiz_results.isNotEmpty()) {
+                for (item in user.quiz_results.last().quiz_result_items) {
+                    Row {
+                        Text(item.category.name + ": ")
+                        Text(item.percentage.toString() + '%')
+                    }
                 }
             }
         }
     }
 }
 
-//@Composable
-//fun Categories(categories: SingleResult) {
-//    LazyColumn {
-//        items(categories.quiz_result_items) {
-//            CategoryItem(it)
-//        }
-//    }
-//}
+@Composable
+fun Logo(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(120.dp)
+            .padding(end = 100.dp)
+        ,
+        painter = painterResource(
+            id = if (isSystemInDarkTheme()) R.drawable.logo
+            else R.drawable.logo
+        ),
+        contentDescription = null,
+        contentScale = ContentScale.FillWidth
+    )
+}
 
 @Composable
-fun CategoryItem(categoryItem: CategoryAndPercent) {
-    Row {
-        Text(text = categoryItem.category.name)
-        Text(text = categoryItem.percentage.toString() + '%')
+private fun Header() {
+    Box(modifier = Modifier
+        .padding(horizontal = 20.dp)
+        .fillMaxWidth()) {
+        Text(
+            text = "Студенты",
+            fontWeight = FontWeight.Bold,
+            fontSize = 34.sp
+        )
+
+        Image(
+            painter = painterResource(
+                id = if (isSystemInDarkTheme()) R.drawable.logo
+                else R.drawable.logo
+            ),
+            contentDescription = null,
+            modifier = Modifier.align(Alignment.TopEnd)
+        )
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     GreenLabQuizTheme {
--- greenlabadmin/src/main/java/ru/greenlab/greenlabadmin/MainViewModel.kt
+++ greenlabadmin/src/main/java/ru/greenlab/greenlabadmin/MainViewModel.kt
@@ -5,7 +5,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import retrofit2.awaitResponse
 import retrofit2.create
@@ -26,10 +30,20 @@ class MainViewModel @Inject constructor() : ViewModel() {
 
     val users = MutableStateFlow<List<User>>(emptyList())
 
+    private val latestUsersFlow: Flow<List<User>> = flow {
+        while(true) {
+            val latestUsers = service.listUsers().awaitResponse().body() ?: emptyList()
+            emit(latestUsers) // Emits the result of the request to the flow
+            delay(5000) // Suspends the coroutine for some time
+        }
+    }
+
+
     init {
         viewModelScope.launch(coroutineHandler) {
-            val response = service.listUsers().awaitResponse()
-            users.value = response.body() ?: emptyList()
+            latestUsersFlow.collect() {
+                users.value = it
+            }
         }
     }
 }
